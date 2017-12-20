ab13886b084c2a5718504f87e6cb98ec6e996a9c
weizhang240 <weizhang240@creditease.cn>
2017-12-20 14:22:02 +0800

    配置单独日志可打印

--- my-test-web/src/main/java/com/my/test/web/interceptor/CountInterceptor.java
+++ my-test-web/src/main/java/com/my/test/web/interceptor/CountInterceptor.java
@@ -1,6 +1,9 @@
 package com.my.test.web.interceptor;
 
 import com.my.test.service.person.service.PersonService;
+import com.my.test.web.person.PersonCtl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
  * Created by vivi on 2017/12/10.
  */
 public class CountInterceptor implements HandlerInterceptor {
+    private static final Logger LOGGER = LoggerFactory.getLogger("com.my.test.web.interceptor");
 
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
@@ -25,6 +29,6 @@ public class CountInterceptor implements HandlerInterceptor {
 
     @Override
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-        System.out.println(httpServletRequest.getRequestURI()+"来了一次访问");
+        LOGGER.info("访问路径=====================>{}",httpServletRequest.getRequestURI());
     }
 }
